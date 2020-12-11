3fa289fd5fdfe3e85bded5b4e164ea6677d73461
Tom McLaughlan <tomxmclaughlan@gmail.com>
2020-12-11 10:18:19 +0000

    Day11

--- src/main/java/com/mclaughlan/advent/Main.java
+++ src/main/java/com/mclaughlan/advent/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Solution solution = new Day10();
+        Solution solution = new Day11();
         solution.run();
     }
 }
