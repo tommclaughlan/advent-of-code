c131aa95808f8dc5e48e2dca02a15530a84cb375
Tom McLaughlan <tomxmclaughlan@gmail.com>
2021-12-13 17:14:41 +0000

    Day 12 and 13 - catching up

---
+++ 2021/day12.py
@@ -0,0 +1,77 @@
+from util.runner import Runner
+
+
+def get_connected(connections, node):
+    for i in connections:
+        if node != 'start' and (i[0] == 'start' or i[1] == 'start'):
+            continue
+
+        if node == i[0]:
+            yield i[1]
+        elif node == i[1]:
+            yield i[0]
+
+
+def get_paths(connections):
+    paths = [['start']]
+    done_paths = []
+    while True:
+        next_paths = []
+        for i in paths:
+            for n in get_connected(connections, i[-1]):
+                if n == 'end':
+                    done_paths.append(i + [n])
+                elif n.isupper() or (n.islower() and n not in i):
+                    next_paths.append(i + [n])
+        if len(next_paths) == 0:
+            return done_paths
+        else:
+            paths = next_paths
+
+
+def validate_node(path: list, next_node):
+    for n in path:
+        if n.islower() and path.count(n) > 1 and next_node in path:
+            return False
+
+    return True
+
+
+def get_paths_part2(connections):
+    paths = [['start']]
+    done_paths = []
+    while True:
+        next_paths = []
+        for i in paths:
+            for n in get_connected(connections, i[-1]):
+                if n == 'end':
+                    done_paths.append(i + [n])
+                elif n.isupper() or validate_node(i, n):
+                    next_paths.append(i + [n])
+        if len(next_paths) == 0:
+            return done_paths
+        else:
+            paths = next_paths
+
+
+class Day(Runner):
+    def part1(self):
+        connections = []
+        for c in self.input:
+            connections.append(c.strip().split('-'))
+
+        paths = get_paths(connections)
+
+        return len(paths)
+
+    def part2(self):
+        connections = []
+        for c in self.input:
+            connections.append(c.strip().split('-'))
+
+        paths = get_paths_part2(connections)
+
+        return len(paths)
+
+
+Day(12)
---
+++ 2021/day13.py
@@ -0,0 +1,81 @@
+from util.runner import Runner
+
+import numpy as np
+
+
+def x_fold(grid: 'np.ndarray[np.int]', coord):
+    a = grid[:, :coord]
+    b = grid[:, coord + 1:]
+
+    for x in range(a.shape[1]):
+        for y in range(a.shape[0]):
+            a[y, coord - x - 1] = a[y, coord - x - 1] + b[y, x]
+
+    return a
+
+
+def y_fold(grid: 'np.ndarray[np.int]', coord):
+    a = grid[:coord, :]
+    b = grid[coord + 1:, :]
+
+    for x in range(a.shape[1]):
+        for y in range(a.shape[0]):
+            a[coord - y - 1, x] = a[coord - y - 1, x] + b[y, x]
+
+    return a
+
+
+class Day(Runner):
+    def parse_input(self):
+        # just needs to be 'big enough' - the first couple of folds will give us the right size
+        grid = np.zeros(shape=(1500, 1500))
+        folds = []
+        fill_grid = True
+        for line in self.input:
+            if line.strip() == "":
+                fill_grid = False
+                continue
+            if fill_grid:
+                coords = [int(c) for c in line.strip().split(',')]
+                grid[coords[1], coords[0]] = 1
+            else:
+                fold = line.split('fold along ')[1].split('=')
+                folds.append(fold)
+
+        return grid, folds
+
+    def part1(self):
+        grid, folds = self.parse_input()
+
+        if folds[0][0] == 'x':
+            grid = x_fold(grid, int(folds[0][1]))
+        elif folds[0][0] == 'y':
+            grid = y_fold(grid, int(folds[0][1]))
+
+        return np.count_nonzero(grid[grid > 0])
+
+    def part2(self):
+        grid, folds = self.parse_input()
+
+        for f in folds:
+            if f[0] == 'x':
+                grid = x_fold(grid, int(f[1]))
+            elif f[0] == 'y':
+                grid = y_fold(grid, int(f[1]))
+
+        grid[grid > 0] = 1
+
+        for y in range(grid.shape[0]):
+            line = ''
+            for x in range(grid.shape[1]):
+                if grid[y, x] > 0:
+                    line += '##'
+                else:
+                    line += '  '
+            print(line)
+
+        # this gives...
+        return 'RHALRCRA'
+
+
+Day(13)
