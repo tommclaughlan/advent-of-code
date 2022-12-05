e352d82de8ff9a9108cc681d9b34530716be8313
Tom McLaughlan <tomxmclaughlan@gmail.com>
2022-12-05 22:01:03 +0000

    Day 5 and the inefficient iterators

---
+++ 2022/aoc-2022-day5/src/main.rs
@@ -0,0 +1,125 @@
+use aoc_2022_common::read_lines;
+
+fn main() {
+    let part1_result = part1("../inputs/day5.txt");
+    let part2_result = part2("../inputs/day5.txt");
+
+    println!("Part 1: {}", part1_result);
+    println!("Part 2: {}", part2_result);
+}
+
+fn part1(filename: &str) -> String {
+    let lines = read_lines(filename);
+    let mut iter = lines.iter();
+
+    let initial: Vec<&String> = iter.by_ref().take_while(|l| !l.is_empty()).collect();
+    let moves: Vec<&String> = iter.collect();
+
+    let mut stacks = read_initial_state(initial);
+    
+    for move_string in moves {
+        let (amount, from, to) = parse_move(move_string);
+        for _ in 0..amount {
+            let i = from as usize;
+            let j = to as usize;
+            let item = stacks[i].pop().unwrap();
+            stacks[j].push(item);
+        }
+    }
+    
+    let mut result = String::new();
+
+    for mut stack in stacks {
+        result.push_str(stack.pop().unwrap().to_string().as_str());
+    }
+    
+    return result;
+}
+
+fn part2(filename: &str) -> String {
+    let lines = read_lines(filename);
+    let mut iter = lines.iter();
+
+    let initial: Vec<&String> = iter.by_ref().take_while(|l| !l.is_empty()).collect();
+    let moves: Vec<&String> = iter.collect();
+
+    let mut stacks = read_initial_state(initial);
+
+    for move_string in moves {
+        let (amount, from, to) = parse_move(move_string);
+        let mut intermediate_stack: Vec<char> = Vec::new();
+        let mut target = 0;
+        for _ in 0..amount {
+            let i = from as usize;
+            target = to as usize;
+            let item = stacks[i].pop().unwrap();
+            intermediate_stack.push(item);
+        }
+        intermediate_stack.iter().rev().for_each(|thing| stacks[target].push(*thing));
+        intermediate_stack.clear();
+    }
+
+    let mut result = String::new();
+
+    for mut stack in stacks {
+        result.push_str(stack.pop().unwrap().to_string().as_str());
+    }
+
+    return result;
+}
+
+fn read_initial_state(initial: Vec<&String>) -> Vec<Vec<char>> {
+    let n_stacks = ((initial[0].len() as f32) / 4.0).ceil() as i32;
+    
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+    for _ in 0..n_stacks {
+        stacks.push(Vec::new());
+    }
+    
+    for line in initial {
+        if line.chars().nth(1).unwrap() != '1' {
+            for _i in 0..n_stacks {
+                let index: usize = _i as usize;
+                let item = line.chars().nth(index*4 + 1).unwrap();
+                if item != ' ' {
+                    stacks[index].insert(0, item);
+                }
+            }
+        }
+    }
+    
+    return stacks;
+}
+
+fn parse_move(string: &String) -> (i32, i32, i32) {
+    let mut split = string.split(" ");
+    let amount = split.nth(1).unwrap().parse::<i32>().unwrap();
+    let from = split.nth(1).unwrap().parse::<i32>().unwrap();
+    let to = split.nth(1).unwrap().parse::<i32>().unwrap();
+    return (amount, from - 1, to - 1);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("../inputs/test_input/day5.txt"), "CMZ");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("../inputs/test_input/day5.txt"), "MCD");
+    }
+
+    #[test]
+    fn answer_part1() {
+        assert_eq!(part1("../inputs/day5.txt"), "CVCWCRTVQ");
+    }
+
+    #[test]
+    fn answer_part2() {
+        assert_eq!(part2("../inputs/day5.txt"), "CNSCZWLVT");
+    }
+}
